62751849e08004ff55ed4f68d6a2c8e7c410d879
a lan <bluenowte97@gmail.com>
2018-11-07 17:31:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -76,6 +76,18 @@ end
   #   swapped
   end
 
+  def swap_elements_from_to(array, index, new_index)
+    array[index], array[new_index] = array[new_index], array[index]
+    return array
+  end
+
+  describe 'swap_elements_from_to' do
+    it 'swaps elements and allows you to specify the index of the element you would like to move to a new index' do
+      expect(swap_elements_from_to(["one", "two", "three"], 2, 1)).to eq(["one", "three", "two"])
+    end
+  end
+
+
   def reverse_array(array)
     array.reverse
   end
@@ -103,3 +115,19 @@ def sum_array(numbers)
     accumulator + element
   end
 end
+
+def add_s(array)
+  #Build a method that adds an "s" to each word in the array 
+  #except for the second element in the array 
+  #("feet" is already plural).
+  plurals = []
+      array.each_with_index do |word,index|
+        if index != 1 
+          word = word + "s"
+        end
+        plurals << word 
+  end
+  
+  plurals
+end
+
