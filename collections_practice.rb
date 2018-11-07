797735c17324ac87b2e0e4486731e7dfe30f7ae8
a lan <bluenowte97@gmail.com>
2018-11-07 14:07:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,9 +7,28 @@
 def sort_array_asc(array)
   sorted = []
   i = 0
-  while i < array.length - 1
-    sorted.push(yield [i] <=> array[i + 1])
-    i+= 1
-  end
+  while array.length != 0
+    sorted << array.min
+    array.delete(array.min)
+    #can call min
+    # then delete from array until length = 0
+    end
   sorted
 end
+def sort_array_asc(array)
+  sorted = []
+pivot = array.shift
+  less, more = array.partition do |e| 
+    e < pivot 
+  end  
+#here he makes two groups
+  sorted << sort_array_asc(less) 
+  sorted << [pivot] 
+  sorted << sort_array_asc(more)
+#here above is the recursion
+
+end
+
+
+
+
